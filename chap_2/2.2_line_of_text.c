704270726bb62552b6668e522ddf06077437cbd2
sirmaxford <sirmaxford@gmail.com>
2021-03-31 02:52:37 +0900

    initial commit

---
+++ chap_2/2.2_line_of_text.c
@@ -0,0 +1,9 @@
+// print a line of text
+
+#include <stdio.h> // processed by preprocessor before compilation
+
+// function main begins program execution
+int main(void) // part of every C program. Here, "void" means that main does not receive any info
+{
+    printf("Welcome to C!\n");
+}// end of function main
---
+++ chap_2/2.3_adding_ints.c
@@ -0,0 +1,33 @@
+// Addition program
+
+#include <stdio.h>
+
+int main(void)
+{
+    int integer1;
+    int integer2;
+
+    printf("Enter first integer\n"); // prompt
+    scanf("%d", &integer1); // read the integer input
+
+    printf("Enter second integer\n");
+    scanf("%d", &integer2);
+
+    int sum;
+    sum = integer1 + integer2; // assign total to sum
+
+    printf("Sum is %d\n", sum);
+}
+
+/*
+Notes:
+
+> You may place variable definition anywhere in main.
+> You should define variables close to their first use.
+> C is case sensitive.
+> A self-documenting program is more self-explanatory. Requires few comments.
+> "f" in scanf or printf stands for "formatted".
+> "%d" is a conversion specifier. "%" -- begining of conv specifier, "d" -- decimal integer.
+> "&" is the address operator to specify the location for data to be stored.
+> "=, +" are binary operators. Place spaces on either side for clarity.
+*/
